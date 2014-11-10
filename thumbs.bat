78d5319d06ea5e61bdc9dc83d5f0a9bc36c60894
tostercx <tostercx@gmail.com>
2014-11-11 00:19:34 +0200

    pass tbsd_openjpeg_static from batch thumbs

--- thumbs.bat
+++ thumbs.bat
@@ -18,7 +18,7 @@ rem copy all known env vars to bash
 setlocal enableDelayedExpansion
 set exports=
 
-for %%i in (tbs_conf tbs_arch tbs_tools tbs_static_runtime) do (
+for %%i in (tbs_conf tbs_arch tbs_tools tbs_static_runtime tbsd_openjpeg_static) do (
   if not [!%%i!]==[] (
     set exports=!exports!export %%i=!%%i!;
   )
